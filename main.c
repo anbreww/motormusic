38fd9bf8cb82791fccf8aefe8bbb56fbe17b732e
Andrew Watson <andy@watsons.ch>
2010-06-07 17:51:01 +0200

    add test program
    loads some values from a lookup table at a given speed and outputs them to
    debug port (in this case port A)

--- main.c
+++ main.c
@@ -9,12 +9,48 @@
 
 #include <avr/io.h>
 #include "config.h"
-//#include "music.h"
+#include "music.h"
+
+#include <util/delay.h>
+
+#include <avr/pgmspace.h>
+
+#define DEBUG_DDR   DDRA
+#define DEBUG_PORT  PORTA
+#define DEBUG_PINS  PINA
+
+#define TEMPO   500 /* in ms */
 
 int main(void)
 {
-    // init
+    // init i/o
+    DEBUG_PORT = 0x00;
+    DEBUG_DDR = 0xFF;
+    // init timers
+    // setup pointers? (to melody and notes)
+    int i;
+    uint8_t note, duration;
+    uint16_t freq;
+
+    for(i = 0; i < MELODY_LENGTH; i++)
+    {
+        // load note
+        note = pgm_read_byte(melody[i][0]);
+        freq = pgm_read_word(notes[note]);
+        duration = pgm_read_byte(melody[i][1]);
+
+        while (duration > 0)
+        {
+            DEBUG_PORT = (freq & 0xFF);
+            _delay_ms(TEMPO);
+            duration--;
+        }
+    }
+
+
 
     // main
+
+    return 0;
     
 }
---
+++ music.h
@@ -0,0 +1,67 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include <avr/pgmspace.h>
+
+/* note frequency constants */
+const uint16_t notes[12] PROGMEM = {
+    0x01,
+    0x02,
+    0x04,
+    0x08,
+    0x10,
+    0x20,
+    0x40,
+    0x80,
+    0x0a,
+    0x05,
+    0xa0,
+    0x50
+};
+
+/* these keys refer to notes in the previous table */
+typedef enum NOTES_enum
+{
+    A1 = 0,
+    B1 = 1,
+    C1 = 2,
+    D1 = 3,
+    E1 = 4,
+    F1 = 5,
+    G1 = 6,
+
+    A2 = 7,
+    B2 = 8,
+    C2 = 9,
+    D2 = 10,
+    E2 = 11,
+    F2 = 12,
+    G2 = 13,
+} NOTES_t;
+
+
+/* 
+ * {note, duration}, where note is a position in the notes[] table,
+ * and duration is in n*tempo duration
+ */
+#define MELODY_LENGTH   5
+const uint8_t melody[MELODY_LENGTH][2] PROGMEM = {
+    {A1, 2},
+    {B1, 10},
+    {C2, 5},
+    {F1, 1},
+    {A2, 1}
+};
+
+/*
+ * how to read:
+ * #define NUM 4
+ * note = pgm_read_byte_far(melody[NUM][0]);
+ * duration = pgm_read_byte_far(melody[NUM[1]);
+ *
+ * for 16-bit integers:
+ * pgm_read_word_far(notes[2]);
+ *
+ */
+
+#endif
