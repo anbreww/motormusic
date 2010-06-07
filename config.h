69b4ac46f5d604cf456443425322d971eb08e71d
Andrew Watson <andy@watsons.ch>
2010-06-07 13:54:53 +0200

    add base files

---
+++ config.h
@@ -0,0 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define F_CPU 8000000UL
+
+#endif
---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * MotorMusic
+ *
+ * Andrew Watson - 2010
+ *
+ * Play basic 1-bit "stereo" music on a pair of DC motors
+ *
+ */
+
+#include <avr/io.h>
+#include "config.h"
+//#include "music.h"
+
+int main(void)
+{
+    // init
+
+    // main
+    
+}
